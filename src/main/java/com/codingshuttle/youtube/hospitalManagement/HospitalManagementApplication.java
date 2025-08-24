ed4103abdfa2aaabf9032a8d1ea14ed989061bb8
mahendra-bisht-singh <102457561+mahendra-bisht-singh@users.noreply.github.com>
2025-08-24 08:05:40 +0530

    first commit

---
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/HospitalManagementApplication.java
@@ -0,0 +1,20 @@
+package com.codingshuttle.youtube.hospitalManagement;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementApplication implements CommandLineRunner{
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementApplication.class, args);
+    }
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub hhhhh
+		
+	}
+
+}
---
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/entity/MyBean.java
@@ -0,0 +1,25 @@
+package com.codingshuttle.youtube.hospitalManagement.entity;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean implements InitializingBean, DisposableBean {
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+    
+
+    
+}
